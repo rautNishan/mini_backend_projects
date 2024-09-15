8a65408f3433f45ca29c1193d6c5fae5a1d4fc8f
Nishan Raut <nishan.raut1709@gmail.com>
2024-09-15 12:01:08 +0545

    Redis Dynamic Module Configuration Complete

--- coupon_system/src/common/cache/cache.module.ts
+++ coupon_system/src/common/cache/cache.module.ts
@@ -1,4 +1,38 @@
-import { Module } from '@nestjs/common';
+import {
+  DynamicModule,
+  ForwardReference,
+  Module,
+  Provider,
+  Type,
+} from '@nestjs/common';
+import { ICache } from './interfaces/cache.interface';
+import { ConfigModule } from '@nestjs/config';
+import { RedisModule } from './redis/redis.module';
+import { RedisConnectionService } from './redis/services/redis.connection.service';
 
 @Module({})
-export class CacheModule {}
+export class CacheModule {
+  public static forRoot(cacheOptions: ICache): DynamicModule {
+    const providers: Provider<any>[] = [];
+    const imports: (
+      | DynamicModule
+      | Type<any>
+      | Promise<DynamicModule>
+      | ForwardReference<any>
+    )[] = [];
+
+    imports.push(ConfigModule);
+    if (cacheOptions.redis) {
+      imports.push(RedisModule);
+      providers.push(RedisConnectionService);
+    }
+
+    return {
+      module: CacheModule,
+      providers: [...providers],
+      exports: [...providers],
+      controllers: [],
+      imports: [...imports],
+    };
+  }
+}
