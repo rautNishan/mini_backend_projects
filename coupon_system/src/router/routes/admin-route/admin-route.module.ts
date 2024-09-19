54fcdc2911ad96c208732cd27e89eba78561c869
Nishan Raut <nishan.raut1709@gmail.com>
2024-09-19 22:25:51 +0545

    Solved Dependency

--- coupon_system/src/router/routes/admin-route/admin-route.module.ts
+++ coupon_system/src/router/routes/admin-route/admin-route.module.ts
@@ -3,9 +3,10 @@ import { AuthCommonModule } from 'src/common/request/auth/auth.module';
 import { AdminModule } from 'src/modules/admin/admin.module';
 import { AuthAdminController } from 'src/modules/auth/controller/auth.admin.controller';
 import { UserAdminController } from 'src/modules/user/controllers/user.admin.controller';
+import { UserModule } from 'src/modules/user/user.module';
 
 @Module({
-  imports: [AdminModule, AuthCommonModule],
+  imports: [AdminModule, UserModule, AuthCommonModule],
   controllers: [AuthAdminController, UserAdminController],
 })
 export class AdminRouteModule {}
